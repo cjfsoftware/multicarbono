353123ac4a4609409cdb04cee169a9d8ebd1ac65
Felipe Joaquim Mendes <felipejoaquimmendes@hotmail.com>
2021-05-13 20:29:27 -0300

    adicionada navbar

--- Multicarbono/Multicarbono/Models/Produto/Produto.cs
+++ Multicarbono/Multicarbono/Models/Produto/Produto.cs
@@ -7,5 +7,22 @@ namespace Multicarbono.Models.Produto
 {
     public class Produto
     {
+
+        public int IdProduto { get; set; }
+
+        public int CodProduto { get; set; }
+
+        public string Descricao { get; set; }
+
+        public char Ativo { get; set; }
+
+        public int NCM_SH { get; set; }
+
+        public int CST { get; set; }
+
+        public int Unidade { get; set; }
+
+        public decimal MyProperty { get; set; }
+
     }
 }
