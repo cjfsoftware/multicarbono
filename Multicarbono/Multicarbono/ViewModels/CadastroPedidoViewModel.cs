8b3250006c856c1d3a6666b54b36a1086f271521
JOAOANTONIOCAVALCANT <JOAOANTONIOCAVALCANT@DESKTOP-FSLJ52N.local>
2021-06-05 22:50:51 -0300

    Add CadastroPedidoViewModel.cs

---
+++ Multicarbono/Multicarbono/ViewModels/CadastroPedidoViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Multicarbono.Models.Cliente;
+using Multicarbono.Models.Pedido;
+using Multicarbono.Models.Transportador;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Multicarbono.ViewModels
+{
+    public class CadastroPedidoViewModel
+    {
+        public Pedido pedido { get; set; }
+        public List<SelectListItem> clientes { get; set; }
+        public List<SelectListItem> transportadores { get; set; }
+}
+}
