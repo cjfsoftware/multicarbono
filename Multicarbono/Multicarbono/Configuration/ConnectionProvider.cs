568b4c33d0b88e632892e86a7041a8931dd6cf9a
Felipe Joaquim Mendes <felipejoaquimmendes@hotmail.com>
2021-06-09 23:37:43 -0300

    merge

---
+++ Multicarbono/Multicarbono/Configuration/ConnectionProvider.cs
@@ -0,0 +1,22 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Multicarbono.Database
+{
+    public class ConnectionProvider
+    {
+
+        private static string connStr = ConfigurationManager.AppSettings["dbConn"];
+
+  
+        public DbConnection CreateInstance()
+        {
+            return new MySqlConnection(connStr);
+        }
+    }
+}
---
+++ Multicarbono/Multicarbono/Configuration/Security.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Multicarbono.Configuration
+{
+    public class Security
+    {
+        readonly IConfiguration configuration;
+
+        public Security(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public string Encrypt(string clearText)
+        {
+            string securityKey = this.configuration.GetValue<String>("EncriptKey");
+
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(securityKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
+        }
+    }
+}
